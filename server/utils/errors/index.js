e82e6958266314db728504d814dc3e608848d1e8
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-08-10 23:17:11 +0530

    update

--- server/utils/errors/index.js
+++
@@ -1,9 +0,0 @@
-const AppError = require("./app-error");
-const ServiceError = require("./service-error");
-const ValidationError = require("./validation-error");
-
-module.exports={
-    ValidationError,
-    ServiceError,
-    AppError,
-}
