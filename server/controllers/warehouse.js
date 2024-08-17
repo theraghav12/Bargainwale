6d8ff448f063d70f762aa7c0445187dd1cfa5b69
Amru2010 <amrutanshjha2010@gmail.com>
2024-08-17 15:42:56 +0530

    Updated warehouse controller
    Segregated update warehouse method into two in which new method is of adding inventory item which was earlier in updateWarehouse
    
    Also added deleteInventoryItem method for selling service and incase any order is cancelled

--- server/controllers/warehouse.js
+++ server/controllers/warehouse.js
@@ -52,7 +52,9 @@ const warehouseController = {
       const warehouses = await Warehouse.find(filter);
 
       if (warehouses.length === 0) {
-        return res.status(404).json({ message: "No warehouses found matching the criteria." });
+        return res
+          .status(404)
+          .json({ message: "No warehouses found matching the criteria." });
       }
       res.status(200).json({ warehouses });
     } catch (error) {
@@ -79,74 +81,161 @@ const warehouseController = {
         return res.status(404).json({ message: "Warehouse not found" });
       }
 
-      const { name, weight, quantity, billType, state, city, itemName } =
-        req.body;
+      const { name, state, city } = req.body;
 
       if (state) {
-        warehouse.state = state;
+        warehouse.location.state = state;
       }
       if (city) {
-        warehouse.city = city;
+        warehouse.location.city = city;
       }
       if (name) {
         warehouse.name = name;
       }
 
-      if (billType && weight && quantity && itemName) {
-        const processItem = (inventoryArray) => {
-          return inventoryArray.find((i) => i.itemName === itemName);
-        };
+      await warehouse.save();
 
-        if (billType === "Virtual Billed") {
-          const existingVirtualInventoryItem = processItem(
-            warehouse.virtualInventory
-          );
-          if (!existingVirtualInventoryItem) {
-            warehouse.virtualInventory.push({
-              itemName,
-              weight,
-              quantity,
-            });
-            warehouse.billedInventory.push({
-              itemName,
-              weight,
-              quantity: 0,
-            });
-          } else {
-            existingVirtualInventoryItem.quantity += quantity;
-          }
+      res.status(200).json({
+        message: "Warehouse details updated successfully",
+        warehouse,
+      });
+    } catch (error) {
+      res.status(400).json({ message: "Error updating warehouse", error });
+    }
+  },
+  updateInventoryItem: async (req, res) => {
+    try {
+      let warehouse = await Warehouse.findById(req.params.id);
+
+      if (!warehouse) {
+        return res.status(404).json({ message: "Warehouse not found" });
+      }
+
+      const { itemName, weight, quantity, billType } = req.body;
+
+      if (!itemName || !weight || !quantity || !billType) {
+        return res
+          .status(400)
+          .json({ message: "Incomplete inventory item details" });
+      }
+
+      const processItem = (inventoryArray) => {
+        return inventoryArray.find((i) => i.itemName === itemName);
+      };
+
+      if (billType === "Virtual Billed") {
+        const existingVirtualInventoryItem = processItem(
+          warehouse.virtualInventory
+        );
+        if (!existingVirtualInventoryItem) {
+          warehouse.virtualInventory.push({
+            itemName,
+            weight,
+            quantity,
+          });
+          warehouse.billedInventory.push({
+            itemName,
+            weight,
+            quantity: 0,
+          });
         } else {
-          const existingVirtualInventoryItem = processItem(
-            warehouse.virtualInventory
-          );
-          const existingBilledInventoryItem = processItem(
-            warehouse.billedInventory
-          );
+          existingVirtualInventoryItem.quantity += quantity;
+        }
+      } else {
+        const existingVirtualInventoryItem = processItem(
+          warehouse.virtualInventory
+        );
+        const existingBilledInventoryItem = processItem(
+          warehouse.billedInventory
+        );
 
-          if (!existingBilledInventoryItem) {
+        if (!existingBilledInventoryItem) {
+          return res.status(400).json({
+            message: "Billing for inventory item that is not virtual",
+          });
+        } else {
+          if (quantity > existingVirtualInventoryItem.quantity) {
             return res.status(400).json({
-              message: "Billing for inventory item that is not virtual",
+              message:
+                "Billing more than what is available in virtual inventory",
             });
-          } else {
-            if (quantity > existingVirtualInventoryItem.quantity) {
-              return res.status(400).json({
-                message: "Billing more than what is there in virtual",
-              });
-            }
-            existingVirtualInventoryItem.quantity -= quantity;
-            existingBilledInventoryItem.quantity += quantity;
           }
+          existingVirtualInventoryItem.quantity -= quantity;
+          existingBilledInventoryItem.quantity += quantity;
         }
       }
 
       await warehouse.save();
 
-      res.status(201).json({
-        message: "Warehouse updated successfully",
+      res.status(200).json({
+        message: "Inventory item added/updated successfully",
         warehouse,
       });
     } catch (error) {
-      res.status(400).json({ message: "Error updating warehouse", error });
+      res
+        .status(400)
+        .json({ message: "Error adding/updating inventory item", error });
+    }
+  },
+  deleteInventoryItem: async (req, res) => {
+    try {
+      const { warehouseId } = req.params;
+      const { itemName, billType, quantity } = req.body;
+
+      const warehouse = await Warehouse.findById(warehouseId);
+
+      if (!warehouse) {
+        return res.status(404).json({ message: "Warehouse not found" });
+      }
+
+      const processItem = (inventoryArray) => {
+        return inventoryArray.find((i) => i.itemName === itemName);
+      };
+
+      if (billType === "Virtual") {
+        const existingVirtualInventoryItem = processItem(
+          warehouse.virtualInventory
+        );
+        if (!existingVirtualInventoryItem) {
+          return res
+            .status(400)
+            .json({ message: "Item not found in virtual inventory" });
+        }
+        if (existingVirtualInventoryItem.quantity < quantity) {
+          return res.status(400).json({
+            message: "Cannot reduce quantity below zero in virtual inventory",
+          });
+        }
+        existingVirtualInventoryItem.quantity -= quantity;
+      } else if (billType === "Billed") {
+        const existingBilledInventoryItem = processItem(
+          warehouse.billedInventory
+        );
+        if (!existingBilledInventoryItem) {
+          return res
+            .status(400)
+            .json({ message: "Item not found in billed inventory" });
+        }
+        if (existingBilledInventoryItem.quantity < quantity) {
+          return res.status(400).json({
+            message: "Cannot reduce quantity below zero in billed inventory",
+          });
+        }
+        existingBilledInventoryItem.quantity -= quantity;
+      } else {
+        return res.status(400).json({ message: "Invalid billType provided" });
+      }
+
+      await warehouse.save();
+
+      res.status(200).json({
+        message: "Inventory item quantity updated successfully",
+        warehouse,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error updating inventory item quantity", error });
     }
   },
   deleteWarehouse: async (req, res) => {
