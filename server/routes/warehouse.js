042fe6b4132e3dc149a4dbd6c0c423ec0e3a6c7f
Amru2010 <amrutanshjha2010@gmail.com>
2024-08-17 16:24:48 +0530

    Added more routes in warehouse

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -8,6 +8,14 @@ router.get('/api/warehouse', warehouseController.getAllWarehouses);
 router.get("/api/warehouse/filter", warehouseController.getWarehouseByFilter);
 router.get('/api/warehouse/:id', warehouseController.getWarehouseById);
 router.put('/api/warehouse/:id', warehouseController.updateWarehouse);
+router.put(
+  "/api/warehouse/updateInventoryItem/:id",
+  warehouseController.updateInventoryItem
+);
+router.put(
+  "/api/warehouse/deleteInventoryItem/:id",
+  warehouseController.deleteInventoryItem
+);
 router.delete('/api/warehouse/:id', warehouseController.deleteWarehouse);
 
 export default router;
