2cf311b794d5db86597ca4b53cec153da2a1fe6d
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-12-14 19:20:41 +0530

    fix: change api and proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,13 +6,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.bargainwale.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
