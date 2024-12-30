73c6e17c9bc0ceb0c936d1e6f833738f3b8b1bcd
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-12-31 00:15:01 +0530

    feat: add attack preventions

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./db/index.js";
+import { rateLimit } from "express-rate-limit";
+import xss from "xss";
+import helmet from "helmet";
+import validator from "validator";
 import userRoutes from "./routes/user.js";
 import orderRoutes from "./routes/order.js";
 import inventoryRoutes from "./routes/warehouse.js";
@@ -31,6 +35,49 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
 }));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 1000,
+    standardHeaders: 'draft-7',
+    validate: { xForwardedForHeader: false }
+});
+
+app.use(limiter);
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+    req.body = JSON.parse(JSON.stringify(req.body), (key, value) =>
+        typeof value === 'string' ? xss(value) : value
+    );
+    req.query = JSON.parse(JSON.stringify(req.query), (key, value) =>
+        typeof value === 'string' ? xss(value) : value
+    );
+    req.params = JSON.parse(JSON.stringify(req.params), (key, value) =>
+        typeof value === 'string' ? xss(value) : value
+    );
+    next();
+});
+
+const sanitizeInput = (data) => {
+    if (typeof data === 'string') {
+        return validator.escape(data);
+    }
+    if (typeof data === 'object' && data !== null) {
+        for (const key in data) {
+            data[key] = sanitizeInput(data[key]);
+        }
+    }
+    return data;
+};
+
+app.use((req, res, next) => {
+    req.body = sanitizeInput(req.body);
+    req.query = sanitizeInput(req.query);
+    req.params = sanitizeInput(req.params);
+    next();
+});
 
 app.use(userRoutes);
 
