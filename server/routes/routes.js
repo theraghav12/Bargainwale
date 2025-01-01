70b5a313e226c0e1b46787a1617b96ac6789ecf9
Rishvant Singh <rishvantsinghpundir@gmail.com>
2025-01-02 00:43:01 +0530

    feat: add clustering and simplify routes

---
+++ server/routes/routes.js
@@ -0,0 +1,35 @@
+import userRoutes from "./user.js";
+import orderRoutes from "./order.js";
+import inventoryRoutes from "./warehouse.js";
+import orgRoutes from "./organization.js";
+import bookingRoutes from "./booking.js";
+import transportRoutes from "./transport.js";
+import manufacturerRoutes from "./manufacturer.js";
+import buyerRoutes from "./buyer.js";
+import itemRoutes from "./items.js";
+import purchaseRoutes from "./purchase.js";
+import saleRoutes from "./sale.js";
+import itempriceRoutes from "./itemprice.js";
+import totalSaleRoutes from "./totalsale.js";
+import itemHistoryRoutes from "./itemHistory.js";
+import mailRoutes from "./mail.js";
+import uploadRoutes from "./excelUpload.js";
+
+export const routes = {
+    userRoutes,
+    orderRoutes,
+    inventoryRoutes,
+    orgRoutes,
+    bookingRoutes,
+    transportRoutes,
+    manufacturerRoutes,
+    buyerRoutes,
+    itemRoutes,
+    purchaseRoutes,
+    saleRoutes,
+    itempriceRoutes,
+    totalSaleRoutes,
+    itemHistoryRoutes,
+    mailRoutes,
+    uploadRoutes,
+};
--- server/server.js
+++ server/server.js
@@ -1,117 +1,95 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 import cors from "cors";
+import http from "http";
+import cluster from "cluster";
+import os from "os";
 import connectDB from "./db/index.js";
 import { rateLimit } from "express-rate-limit";
 import xss from "xss";
 import helmet from "helmet";
 import validator from "validator";
-import userRoutes from "./routes/user.js";
-import orderRoutes from "./routes/order.js";
-import inventoryRoutes from "./routes/warehouse.js";
-import orgRoutes from "./routes/organization.js";
 import setUpJobs from "./utils/jobs.js";
-import bookingRoutes from "./routes/booking.js";
-import transportRoutes from './routes/transport.js';
-import manufacturerRoutes from './routes/manufacturer.js';
-import buyerRoutes from './routes/buyer.js';
-import itemRoutes from './routes/items.js';
-import purchaseRoutes from './routes/purchase.js';
-import saleRoutes from "./routes/sale.js";
-import itempriceRoutes from "./routes/itemprice.js";
-import totalSaleRoutes from "./routes/totalsale.js";
-import itemHistoryRoutes from "./routes/itemHistory.js";
-import mailRoutes from "./routes/mail.js";
-import uploadRoutes from "./routes/excelUpload.js";
-dotenv.config();
+import { routes } from "./routes/routes.js";
 
 const PORT = process.env.PORT || 3000;
-const app = express();
-connectDB();
-app.use(express.json());
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: false,
-}));
+const numCPUs = os.cpus().length;
 
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit: 1000,
-    standardHeaders: 'draft-7',
-    validate: { xForwardedForHeader: false }
-});
+if (cluster.isPrimary) {
+    console.log(`Master process is running. Spawning ${numCPUs} worker processes...`);
 
-app.use(limiter);
+    connectDB();
 
-app.use(helmet());
-
-app.use((req, res, next) => {
-    req.body = JSON.parse(JSON.stringify(req.body), (key, value) =>
-        typeof value === 'string' ? xss(value) : value
-    );
-    req.query = JSON.parse(JSON.stringify(req.query), (key, value) =>
-        typeof value === 'string' ? xss(value) : value
-    );
-    req.params = JSON.parse(JSON.stringify(req.params), (key, value) =>
-        typeof value === 'string' ? xss(value) : value
-    );
-    next();
-});
-
-const sanitizeInput = (data) => {
-    if (typeof data === 'string') {
-        return validator.escape(data);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-    if (typeof data === 'object' && data !== null) {
-        for (const key in data) {
-            data[key] = sanitizeInput(data[key]);
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
+    app.use(express.json());
+
+    app.use(cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: false,
+    }));
+
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000,
+        limit: 1000,
+        standardHeaders: 'draft-7',
+        validate: { xForwardedForHeader: false }
+    });
+
+    app.use(limiter);
+
+    app.use(helmet());
+
+    app.use((req, res, next) => {
+        req.body = JSON.parse(JSON.stringify(req.body), (key, value) =>
+            typeof value === 'string' ? xss(value) : value
+        );
+        req.query = JSON.parse(JSON.stringify(req.query), (key, value) =>
+            typeof value === 'string' ? xss(value) : value
+        );
+        req.params = JSON.parse(JSON.stringify(req.params), (key, value) =>
+            typeof value === 'string' ? xss(value) : value
+        );
+        next();
+    });
+
+    const sanitizeInput = (data) => {
+        if (typeof data === 'string') {
+            return validator.escape(data);
         }
-    }
-    return data;
-};
+        if (typeof data === 'object' && data !== null) {
+            for (const key in data) {
+                data[key] = sanitizeInput(data[key]);
+            }
+        }
+        return data;
+    };
 
-app.use((req, res, next) => {
-    req.body = sanitizeInput(req.body);
-    req.query = sanitizeInput(req.query);
-    req.params = sanitizeInput(req.params);
-    next();
-});
+    app.use((req, res, next) => {
+        req.body = sanitizeInput(req.body);
+        req.query = sanitizeInput(req.query);
+        req.params = sanitizeInput(req.params);
+        next();
+    });
 
-app.use(userRoutes);
+    Object.values(routes).forEach((route) => {
+        app.use(route);
+    });
 
-app.use(orderRoutes);
+    const server = http.createServer(app);
 
-app.use(inventoryRoutes);
-
-app.use(orgRoutes);
-
-app.use(bookingRoutes);
-
-app.use(manufacturerRoutes);
-
-app.use(buyerRoutes);
-
-app.use(itemRoutes);
-
-app.use(transportRoutes);
-
-app.use(purchaseRoutes);
-
-app.use(saleRoutes);
-
-app.use(itempriceRoutes);
-
-app.use(totalSaleRoutes);
-
-app.use(itemHistoryRoutes);
-
-app.use(mailRoutes);
-
-app.use(uploadRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server listening to PORT ${PORT}`);
-    setUpJobs();
-});
+    server.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is running on http://localhost:${PORT}`);
+        setUpJobs();
+    });
+}
